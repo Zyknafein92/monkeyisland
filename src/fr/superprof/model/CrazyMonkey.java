deea01cdfd42a86ede3c9ba59f3c0489f6feecce
Zyk <deneux.j@gmail.com>
2018-10-12 04:09:02 +0100

    Add CrazyMonkey

--- src/fr/superprof/model/CrazyMonkey.java
+++ src/fr/superprof/model/CrazyMonkey.java
@@ -1,5 +1,7 @@
 package fr.superprof.model;
 
+import java.util.Random;
+
 public class CrazyMonkey extends Monkey {
 
     public static final Integer SPEED = 1;
@@ -8,13 +10,45 @@ public class CrazyMonkey extends Monkey {
         super(cell);
     }
 
-    public Cell getRandomAdjacentCell(Cell cell){
-        return null;
-    }
+    public Cell getRandomAdjacentCell(Cell cell) {
+
+        int row, col, roll;
+        Cell adjacentCell = cell;
+        Random r = new Random();
+        roll = r.nextInt(4);
+
+        switch (roll) {
+            case 0:
+                row = cell.getRow() - SPEED;
+                col = cell.getCol();
+                break;
+            case 1:
+                row = cell.getRow() + SPEED;
+                col = cell.getCol();
+                break;
+            case 2:
+                row = cell.getRow();
+                col = cell.getCol() - SPEED;
+                break;
+            case 3:
+                row = cell.getRow();
+                col = cell.getCol() + SPEED;
+                break;
+            default:
+                break;
+        }
+            return adjacentCell;
+        }
+
 
     @Override
     public void behavior() {
-        //TODO
+     Cell moveCrazyMonkey = null;
+     do {
+         moveCrazyMonkey = getRandomAdjacentCell(this.getCell());
+         if(moveCrazyMonkey.canAccess() == false) moveCrazyMonkey = null;
+     } while(moveCrazyMonkey == null);
+     this.moveTo(moveCrazyMonkey);
     }
 
     @Override
